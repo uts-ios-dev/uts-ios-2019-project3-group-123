2773b55df85604dfa7140ece96edf0fb989585fa
Alex Lin <peiming.lin@student.uts.edu.au>
2019-05-15 16:34:58 +1000

    Project created

---
+++ FoodTinder/FoodTinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTinder
+//
+//  Created by Alex Lin on 15/5/19.
+//  Copyright © 2019 Alex Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
