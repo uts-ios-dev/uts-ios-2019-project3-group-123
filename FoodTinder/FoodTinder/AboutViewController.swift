1ac507efd6e0a830be6adc2b8722b7c3d2357627
ZS <12582108@uts.edu.au>
2019-05-22 15:29:46 +1000

    Adds about view controller class
    Adds dismiss button action, and hooks up outlet.

---
+++ FoodTinder/FoodTinder/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  FoodTinder
+//
+//  Created by Zachary Simone on 22/5/19.
+//  Copyright © 2019 Alex Lin. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func didTapBack(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
